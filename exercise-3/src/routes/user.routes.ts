2d5622d213302038a10b94d92c1604e72c6b9224
elyse502 <elyseniyibizi502@gmail.com>
2026-06-13 09:11:58 +0200

    feat(routes): configure user api routes
        - register user creation route
        - register user retrieval routes
        - register user deletion route

---
+++ exercise-3/src/routes/user.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { UserController } from "../controllers/user.controller.js";
+
+const router = Router();
+
+// health check endpoint
+router.post("/", (_req, res) => {
+  res.status(200).json({
+    message: "Welcome to the User API 🍽️",
+  });
+});
+
+router.post("/users", UserController.createUser);
+
+router.get("/users", UserController.getUsers);
+
+router.get("/users/:id", UserController.getUserById);
+
+router.delete("/users/:id", UserController.deleteUser);
+
+export default router;
