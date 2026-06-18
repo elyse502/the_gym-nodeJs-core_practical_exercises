32fd5371d3a17a212e2adfb025870eea532eeb16
elyse502 <elyseniyibizi502@gmail.com>
2026-06-18 07:53:26 +0200

    chore: update dev script to target log-scheduling exercise
        - Change dev script from 4b-5-control-order to 4b-4-log-scheduling/server.ts
        - Add server start log message with listening URL
        - Display port and localhost link for easy access
        - Maintain existing server functionality

--- exercise-4/src/4b-4-log-scheduling/server.ts
+++ exercise-4/src/4b-4-log-scheduling/server.ts
@@ -9,4 +9,6 @@ const server = http.createServer((_req, res) => {
   });
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("server listening on port 3000 => http://localhost:3000");
+});
